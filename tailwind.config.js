62f897be62d68db1998bda120570b4ac0b1cc5af
Glenn Dixon <donblanco@posteo.net>
2020-05-03 13:43:17 -0500

    update to Tailwind 1.4.4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,41 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  theme: {
-    // Replaces default settings or add new settings
-    container: {
-      center: true,
+    purge: {
+      content: ["./dist/**/*.html"],
+      whitelist: ["dark-mode"],
     },
-    darkSelector: ".dark-mode",
-    // Extends default settings
-    extend: {
-      fontFamily: {
-        // Use Tailwind default serif fonts with font-display
-        display: [...defaultTheme.fontFamily.serif],
+
+    theme: {
+        // Replaces default settings or add new settings
+        container: {
+          center: true,
+        },
+        darkSelector: ".dark-mode",
+        // Extends default settings
+        extend: {
+          fontFamily: {
+            // Use Tailwind default serif fonts with font-display
+            display: [...defaultTheme.fontFamily.serif],
+          },
+          colors: {
+            bodybg: "var(--color-bodybg)",
+            muted: "var(--color-muted)",
+            bodytext: "var(--color-bodytext)",
+            primary: "var(--color-primary)",
+            secondary: "var(--color-secondary)",
+            links: "var(--color-links)",
+            focus: "var(--color-focus)",
+          },
+        },
       },
-      colors: {
-        bodybg: "var(--color-bodybg)",
-        muted: "var(--color-muted)",
-        bodytext: "var(--color-bodytext)",
-        primary: "var(--color-primary)",
-        secondary: "var(--color-secondary)",
-        links: "var(--color-links)",
-        focus: "var(--color-focus)",
+      variants: {
+        backgroundColor: ["hover", "focus", "dark", "dark-hover", "dark-focus"],
+        borderColor: ["hover", "focus", "dark", "dark-hover", "dark-focus"],
+        textColor: ["hover", "focus", "dark", "dark-hover", "dark-focus"],
       },
-    },
-  },
-  variants: {
-    backgroundColor: ["hover", "focus", "dark", "dark-hover", "dark-focus"],
-    borderColor: ["hover", "focus", "dark", "dark-hover", "dark-focus"],
-    textColor: ["hover", "focus", "dark", "dark-hover", "dark-focus"],
-  },
-  plugins: [
-    require("@tailwindcss/ui"), // https://tailwindui.com/components
-    require("tailwindcss-dark-mode")(), // https://github.com/ChanceArthur/tailwindcss-dark-mode
-  ],
-};
+      plugins: [
+        require("@tailwindcss/ui"), // https://tailwindui.com/components
+        require("tailwindcss-dark-mode")(), // https://github.com/ChanceArthur/tailwindcss-dark-mode
+      ],
+  }
